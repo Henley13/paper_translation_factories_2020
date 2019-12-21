9256cfba53f18373c354788c8e4cc69f44e09c83
Arthur Imbert <arthur.imbert.pro@gmail.com>
2019-12-21 15:46:03 +0100

    add nucleAIzer code

---
+++ nucleAIzer/biomagdsb/start_prediction_fast.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# EDIT these paths to your directories, respectively:
+
+# MaskRCNN folder (delete the word "#" from the beginning of the line and set the path if you already have
+# a MaskRCNN folder - in this case, move the word "#" from between the beginning of the last 2 lines):
+PATHTOINSERT="../Mask_RCNN"
+
+# working directory where you downloaded the code and will have the output under ~\kaggle_workflow\outputs\postprocessing:
+ROOT_DIR="."
+
+# directory of your images to segment:
+IMAGES_DIR="testImages"
+# -----------------------------------------------------------------------------
+
+
+# --- DO NOT EDIT from here ---
+source run_workflow_predictOnly_fast.sh $ROOT_DIR $IMAGES_DIR $PATHTOINSERT
+# source run_workflow_predictOnly_fast.sh $ROOT_DIR $IMAGES_DIR
